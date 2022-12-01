7a1390a9add1294f3ebff8ef9f0ff8c88b97de65
maksim <sairus.89@mail.ru>
2022-12-01 10:27:25 +0300

    innosilicon_api_problem

--- innosilicon_api.py
+++ innosilicon_api.py
@@ -1,13 +1,18 @@
 from dragon_rest.dragons import DragonAPI
 
-dragon_host = ''
+miners = [
+    '192.168.1.68',
+    '192.168.1.114'
+]
+
+dragon_host = miners[1]
 api = DragonAPI(
     dragon_host,
     username='admin',
-    password='dragonadmin'
+    password='KX26d'
 )
 
-r = api.summary()
-print(r)
 
-
+if __name__ == '__main__':
+    r = api.summary()
+    print(r)
