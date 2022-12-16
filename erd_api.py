4c9d8127ae7bc7f9b5d59dbc122af6c5e4b7369f
maksim <sairus.89@mail.ru>
2022-12-16 13:40:53 +0300

    try_snmp

--- erd_api.py
+++ erd_api.py
@@ -5,21 +5,51 @@ from pysnmp.hlapi import *
 from pysnmp.entity.rfc3413.oneliner import cmdgen
 
 
-class Device:
+def get_value():
+    iterator = getCmd(
+        SnmpEngine(),
+        CommunityData('public', mpModel=0),
+        UdpTransportTarget(('192.168.1.2', 161)),
+        ContextData(),
+        ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0))
+    )
 
-    def __init__(self, ip, ro, oids, port=161):
-        self.ip = ip
-        self.ro = ro
-        self.oids = oids
-        self.port = port
-        self.if_oids = ['ifAdminStatus', 'ifOperStatus', 'ifInOctets', 'ifOutOctets']
+    errorIndication, errorStatus, errorIndex, varBinds = next(iterator)
 
-    def get_ifwalk(self):
-        pass
+    if errorIndication:
+        print(errorIndication)
+
+    elif errorStatus:
+        print('%s at %s' % (errorStatus.prettyPrint(),
+                            errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+
+    else:
+        for varBind in varBinds:
+            print(' = '.join([x.prettyPrint() for x in varBind]))
+
+
+def send_trap():
+    iterator = sendNotification(
+        SnmpEngine(),
+        CommunityData('public', mpModel=0),
+        UdpTransportTarget(('192.168.1.2', 162)),
+        ContextData(),
+        'trap',
+        NotificationType(
+            ObjectIdentity('1.3.6.1.6.3.1.1.5.2')
+        ).addVarBinds(
+            ('1.3.6.1.6.3.1.1.4.3.0', '1.3.6.1.4.1.20408.4.1.1.2'),
+            ('1.3.6.1.2.1.1.1.0', OctetString('my system'))
+        ).loadMibs(
+            'SNMPv2-MIB'
+        )
+    )
+
+    errorIndication, errorStatus, errorIndex, varBinds = next(iterator)
+
+    if errorIndication:
+        print(errorIndication)
 
 
 if __name__ == "__main__":
-    name_script, ip, ro, oid = argv
-    device = Device('192.168.1.2', ro, oid)
-
-    print(json.dumps(device.get_ifwalk()))
+    get_value()
