f6eeca9d49e146502191477404a4eb37b42fe4e8
maksim <sairus.89@mail.ru>
2022-12-26 10:31:47 +0300

    snmp_work

--- erd_api.py
+++ erd_api.py
@@ -52,19 +52,34 @@ def send_trap():
 
 
 def request_one_iod():
-    erd3temperatureSensor0 = '.1.3.6.1.4.1.40418.2.4.4.1.0'
-    # errorIndication, errorStatus, errorIndex, varBindTable = cmdgen.nextCmd((
-    #     cmdgen.CommunityData(ro_community, mpModel=1),
-    #     cmdgen.UdpTransportTarget((net_address, 161)),
-    #     ('1.3.6.1.2.1.2.2.1.7'),
-    #     ('1.3.6.1.2.1.2.2.1.8'),
-    #     ('1.3.6.1.2.1.2.2.1.10'),
-    #     ('1.3.6.1.2.1.2.2.1.16')
-    # )
-    a, b, c, d = cmdgen.nextCmd(
 
-    )
+    target_value = '.1.3.6.1.4.1.40418.2.4.4.1.0'  # Name/OID: erd3temperatureSensor.0; Value (Integer): 26
+    target_value2 = '.1.3.6.1.4.1.40418.2.4.4.2.2.0'  # Name/OID: erd3temperatureSensorOut1.0; Value (Integer): 21
+    target_value3 = '.1.3.6.1.4.1.40418.2.4.2.3.0'  # Name/OID: erd3remoteControlContact11.0; Value (Integer): manON(0)
+    target_value4 = '.1.3.6.1.4.1.40418.2.4.2.1.0'  # Name/OID: erd3resetSmartContact10.0; Value (Integer): bypass(0)
+
+    g = getCmd(SnmpEngine(),
+                CommunityData('public', mpModel=0),
+                UdpTransportTarget(('192.168.1.2', 161)),
+                ContextData(),
+                ObjectType(ObjectIdentity(target_value2)))
+
+    errorIndication, errorStatus, errorIndex, varBinds = next(g)
+
+
+    if errorIndication:
+        print(errorIndication)
+
+    elif errorStatus:
+        print('%s at %s' % (errorStatus.prettyPrint(),
+                            errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+
+    else:
+        for varBind in varBinds:
+            print(' = '.join([x.prettyPrint() for x in varBind]))
 
 
 if __name__ == "__main__":
+    # get_value()
+    # send_trap()
     request_one_iod()
