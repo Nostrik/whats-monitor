763de21dd58f60c9baae365595f2a2a99bf19036
maksim <sairus.89@mail.ru>
2023-04-26 08:28:36 +0300

    nmap file update

--- nmap_api.py
+++ nmap_api.py
@@ -1,10 +1,33 @@
 import subprocess
-import shlex
-import time
-import os
+import argparse
 
-cmd = 'nmap -sP 192.168.0.1/24'
-args = shlex.split(cmd)
 
-if __name__ == "__main__":
-    os.system(cmd)
+def ping_ip(ip_address, count):
+    """
+    Ping IP address and return tuple:
+    On success: (return code = 0, command output)
+    On failure: (return code, error output (stderr))
+    """
+    reply = subprocess.run(
+        f"ping -c {count} -n {ip_address}",
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+    )
+    if reply.returncode == 0:
+        return True, reply.stdout
+    else:
+        return False, reply.stdout + reply.stderr
+
+
+parser = argparse.ArgumentParser(description="Ping script")
+
+parser.add_argument("-a", dest="ip", required=True)
+parser.add_argument("-c", dest="count", default=2, type=int)
+
+args = parser.parse_args()
+print(args)
+
+rc, message = ping_ip(args.ip, args.count)
+print(message)
